f231deba10ea45a3bc4516be1a02fe5e543cccfa
Florian Bruggisser <florian@nexpose.ch>
2017-09-28 01:03:44 +0200

    added basic iteration example

--- prepare.py
+++ prepare.py
@@ -1,7 +1,20 @@
 
+from tinydb import TinyDB, Query
+
+DB_NAME = 'words.json'
+DICTIONARY_NAME = 'data/dictionary.csv'
+
+def analyseDictionary():
+	with open(DICTIONARY_NAME) as f:
+	    for line in f:
+	       print line
 
 def main():
-	print("hello world")
+	print('building word index...')
+
+	db = TinyDB(DB_NAME)
+
+	analyseDictionary()
 
 if __name__ == '__main__':
 	main()
